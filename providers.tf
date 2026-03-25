90f14339e7705002a3a4dabe814bfd4c6fa82ccd
richou <richanelmbou@gmail.com>
2026-03-25 10:19:52 -0400

    pull modif main

--- providers.tf
+++ providers.tf
@@ -1,6 +1,10 @@
 provider "aws" {
 
   region = "eu-west-1"
+  access_key = "${AWS_ACCESS_KEY_ID}"
+  secret_key = "${AWS_SECRET_ACCESS_KEY}"
+  
+  # we have to give a role 
 
   default_tags {
     tags = {
