5cbc9a0e15456a8e2ca6d72328b042c449d697d5
richou <richanelmbou@gmail.com>
2026-03-26 11:33:41 -0400

    feat: user data - k3s

--- modules/ec2/user_data.sh
+++ modules/ec2/user_data.sh
@@ -5,3 +5,9 @@ dnf update
 dnf install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 systemctl enable amazon-ssm-agent
 systemctl start amazon-ssm-agent
+
+# installation k3s
+dnf install -y curl
+curl -sfL https://get.k3s.io | sh -  #ok
+#sudo swapoff -a
+#sudo sed -i '/swap/d' /etc/fstab   #think to open this port 6443, 10250 on security group
