8cbd6177e56c7b51b5666ca0501e7fa84728ca7e
richou <richanelmbou@gmail.com>
2026-03-30 11:19:52 -0400

    fix:alb,rds,aws cli

--- modules/ec2/user_data.sh
+++ modules/ec2/user_data.sh
@@ -16,7 +16,7 @@ curl -sfL https://get.k3s.io | sh -  #ok
 
 # docker runtime for kubernetes
 dnf -y install dnf-plugins-core
-dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repoqex
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 systemctl enable docker
 systemctl start docker
